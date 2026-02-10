303603d1177e039737ccaaf440b3f1279c1f5f50
jamesmo-max <jamesmo570@gmail.com>
2026-02-10 09:42:08 -0500

    Add files via upload

---
+++ Corte1/Teoria/practica3/practica3.py
@@ -0,0 +1,16 @@
+
+b=1
+while (int(b)==1):
+
+   a=input("ingrese numero: ")
+
+   mod= int(a)%2
+   if(mod==0):
+      print("par")
+   else:
+      print("impar")
+   
+   b=input("(1)seguir----(2)salir")
+   #if (int(b)==2):
+    #   break
+        
