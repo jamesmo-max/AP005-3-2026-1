643946c4fff04fa15a5c2c21ac06cefe5a07bce0
jamesmo-max <jamesmo570@gmail.com>
2026-02-11 15:34:21 -0500

    Add files via upload

---
+++ Corte1/teoriaCpp/practica2/suma2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int num1, num2, num3;
+cout<<"ingrese numero 1: ";
+cin>>num1;
+cout<<"ingrese numero 2: ";
+cin>>num2;
+
+num3=num1 + num2;
+cout<<"la suma es" <<num3<<endl;
+return 0;
+}
